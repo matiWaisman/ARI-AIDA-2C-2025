36ba666fe91db2894aaee722f36dc2751b4f3363
Matias <matiasewaisman@gmail.com>
2025-09-08 22:33:38 -0300

    Junte modo LU, faltan un millon de refactors

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,13 @@ import { Client } from 'pg';
 import readline from 'readline';
 import { writeFileSync } from "fs";
 import { insertAlumnos } from './utils/insert-alumnos.ts';
-import { getPrimerAlumnoConCertificado, hayAlgunAlumnoConCertificado, getCertificadoDeAlumnoConLU, hayAlgunAlumnoConFechaDeseada, getAlumnosConFechaDeseada } from './utils/get-alumno-necesita-certificado.ts';
+import { getPrimerAlumnoConCertificado, hayAlgunAlumnoConCertificado, getCertificadoDeAlumnoConLU, hayAlgunAlumnoConFechaDeseada, getAlumnosConFechaDeseada, hayAlumnoConLu } from './utils/get-alumno-necesita-certificado.ts';
 import dotenv from 'dotenv';
 import path from 'path';
 import type { Alumno } from './types/alumno.ts';
 import { generarCertificadoHtml } from './utils/generar-certificado.ts';
 
 dotenv.config({ path: './local-sets.env' });
-
-
-
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -36,52 +33,39 @@ rl.question(`¿Qué modo querés usar? \n
     port: Number(process.env.PGPORT) || 5432,
     database: process.env.PGDATABASE,
   });
-  // Expresiones regulares para cada caso
+
   const regexArchivo = /^--archivo\s+(.+)$/;
   const regexFecha   = /^--fecha\s+(\d{4}-\d{2}-\d{2})$/;
   const regexLu      = /^--lu\s+(\S+)$/;
 
   let match;
   await clientDb.connect(); 
+
   if ((match = input.match(regexArchivo))) {
     console.log("Modo carga");
     console.log("Ruta:", match[1]);
   } else if ((match = input.match(regexFecha))) {
     console.log("Modo fecha");
-    if(await hayAlgunAlumnoConFechaDeseada(clientDb, match[1]!)){
-      console.log("Hay alguno");
-    }
-    else{
-      console.log("No hay ninguno");
+    const fecha: String = match[1]!;
+    if(await hayAlgunAlumnoConFechaDeseada(clientDb, fecha)){
+      console.log(await getAlumnosConFechaDeseada(clientDb, fecha));
+    } else {
+      console.log("No hay ningun alumno que cumpla con la fecha");
     }
   } else if ((match = input.match(regexLu))) {
     console.log("Modo LU");
-    console.log("LU:", match[1]);
+    const LU: String = match[1]!;
+    if(await hayAlumnoConLu(clientDb, LU)){
+      const alumno: Alumno = await getCertificadoDeAlumnoConLU(clientDb, LU);
+      generarCertificadoHtml(alumno);
+      console.log(alumno);
+    } else {
+      console.log("No hay ningun alumno con ese LU");
+    }
   } else {
     console.log("Entrada no reconocida");
   }
 
   rl.close();
-
   await clientDb.end();
 });
-
-
-/*
-await clientDb.connect();
-
-const fileAlumnosPath = path.join(process.cwd(), 'resources', 'alumnos.csv');
-
-await insertAlumnos(clientDb, fileAlumnosPath);
-
-if(await hayAlgunAlumnoConCertificado(clientDb)){
-  console.log("Hay un alumno con certificado");
-  const alumno: Alumno = (await getPrimerAlumnoConCertificado(clientDb))!; // El ! indica que nunca va a ser indefinido
-  const certificadoHtml = generarCertificadoHtml(alumno);
-  
-  writeFileSync("certificado.html", certificadoHtml, "utf-8");
-}
-else {
-  console.log("No hay ningun alumno al que le podamos armar un certificado");
-}
-*/
--- src/utils/get-alumno-necesita-certificado.ts
+++ src/utils/get-alumno-necesita-certificado.ts
@@ -9,28 +9,32 @@ export async function getPrimerAlumnoConCertificado(client: Client): Promise<Alu
     return res;
 }
 
-export async function getCertificadoDeAlumnoConLU(client: Client, LU: String): Promise<Alumno | undefined> {
+export async function getCertificadoDeAlumnoConLU(client: Client, LU: String): Promise<Alumno> {
     let query = "SELECT * FROM aida.alumnos WHERE titulo IS NOT NULL AND lu = $1"
     const queryAlumno = await client.query<Alumno>(query, [LU]);
-    const alumno: Alumno | undefined = queryAlumno.rows[0]
-    return alumno
+    const alumno: Alumno = queryAlumno.rows[0]!; 
+    return alumno;
+}
+
+export async function hayAlumnoConLu(client: Client, LU: String): Promise<boolean> {
+    let query = "SELECT * FROM aida.alumnos WHERE titulo IS NOT NULL AND lu = $1"
+    const queryAlumno = await client.query<Alumno>(query, [LU]);
+    return queryAlumno.rows.length > 0;
 }
 
 export async function hayAlgunAlumnoConCertificado(client: Client): Promise<Boolean> {
     let query = "SELECT * FROM aida.alumnos WHERE titulo IS NOT NULL AND titulo_en_tramite IS NOT NULL ORDER BY egreso LIMIT 1"
     const resDb = await client.query<Alumno>(query);
-    // Si hay respuesta es porque hay alguno, si no hay es porque no hay ninguno
     return (resDb.rows.length > 0);
 }
 
 export async function hayAlgunAlumnoConFechaDeseada(client: Client, fecha: String): Promise<Boolean> {
     const query = `SELECT * FROM aida.alumnos WHERE titulo IS NOT NULL AND titulo_en_tramite IS NOT NULL AND egreso = '${fecha}' ORDER BY egreso LIMIT 1`;
     const resDb = await client.query<Alumno>(query);
-    // Si hay respuesta es porque hay alguno, si no hay es porque no hay ninguno
     return (resDb.rows.length > 0);
 }
 
-export async function getAlumnosConFechaDeseada(client: Client, fecha: Date): Promise<Alumno> {
+export async function getAlumnosConFechaDeseada(client: Client, fecha: String): Promise<Alumno> {
     const query = `SELECT * FROM aida.alumnos WHERE titulo IS NOT NULL AND titulo_en_tramite IS NOT NULL AND egreso = '${fecha}' ORDER BY egreso LIMIT 1`;
     const resDb = await client.query<Alumno>(query);
     const res: Alumno = resDb.rows[0]!; 
