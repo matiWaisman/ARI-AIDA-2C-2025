b13ea07715df08ac5a5e2c4749e16203e8f1ca8f
Leonisi0 <martinleoncuadrado@gmail.com>
2025-12-06 16:54:25 -0300

    wip

--- aida-app/features/registerFeature.ts
+++ aida-app/features/registerFeature.ts
@@ -30,7 +30,7 @@ export function useRegisterFeature() {
     }
 
     try {
-      await apiClient("/register", {
+      await apiClient("/create/user", {
         method: "POST",
         body: JSON.stringify({ 
           username, 
@@ -43,7 +43,34 @@ export function useRegisterFeature() {
           esProfesor 
         }),
       });
-
+      await apiClient("/create/entidadUniversitaria", {
+        method: "POST",
+        body: JSON.stringify({ 
+          nombre,
+          apellido, 
+          lu, 
+        }),
+      });
+      if (esProfesor) { 
+      await apiClient("/create/profesor", {
+        method: "POST",
+        body: JSON.stringify({ 
+          nombre,
+          apellido, 
+          lu,
+        }),
+      });
+      }
+      if (esAlumno) {
+      await apiClient("/create/alumno", {
+        method: "POST",
+        body: JSON.stringify({ 
+          nombre,
+          apellido, 
+          lu, 
+        }),
+      });
+      }
       router.push("/login");
     } catch (err) {
       if (err instanceof Error) setError(err.message);
